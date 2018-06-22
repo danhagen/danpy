287a6237db6ddb32419525172d56afd4d3d3eb59
danhagen <danhagen@users.noreply.github.com>
2018-06-22 12:55:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if path.isfile(readme_filepath):
         
 setuptools.setup(
     name="dsb",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/danhagen/dsb",
     author="Daniel A Hagen",
     author_email="dhagen@usc.edu",
