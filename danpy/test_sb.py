44ab663fdc0a5a79f22ca532ebbaf486df5384c2
dhagen.work <dhagen@usc.edu>
2018-06-25 11:45:16 -0700

    #1 Added a test for the automatic reset that occurs for repeated loops.

--- danpy/test_sb.py
+++ danpy/test_sb.py
@@ -60,3 +60,29 @@ def test_dsb_reset():
         "Error initializing self.time_array. Should be '--', instead of " + str(statusbar.time_left)
 
     del(statusbar,current_time)
+
+def test_dsb_automatic_reset():
+    statusbar = dsb()
+    for i in range(10):
+        time.sleep(0.1)
+        statusbar.update(i,10)
+    statusbar.update(0,10)
+    current_time = time.time()
+
+    assert not hasattr(statusbar,"bar_indices"), "Error resetting dsb(). bar_indices should not exist."
+
+    assert hasattr(statusbar,'counter'), "self.counter not initialized for dsb()"
+    assert hasattr(statusbar,'time_array'), "self.time_array not initialized for dsb()"
+    assert hasattr(statusbar,'start_time'), "self.start_time not initialized for dsb()"
+    assert hasattr(statusbar,'time_left'), "self.time_left not initialized for dsb()"
+
+    assert statusbar.counter == 0, \
+        "Error initializing self.counter for dsb(). Should be 0, instead of " + str(statusbar.counter)
+    assert statusbar.time_array == [], \
+        "Error initializing self.time_array. Should be [], instead of " + str(statusbar.time_array)
+    assert abs(statusbar.start_time - current_time)<1e-4, \
+        "Error initializing self.start_time. Should be " + str(current_time) + ", instead of " + str(statusbar.start_time)
+    assert statusbar.time_left == "--", \
+        "Error initializing self.time_array. Should be '--', instead of " + str(statusbar.time_left)
+
+    del(statusbar,current_time)
