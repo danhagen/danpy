8b470dd14bc93f7817df88b5b3b93f16f6f4762c
dhagen.work <dhagen@usc.edu>
2018-06-25 10:00:32 -0700

    #1 added a test for print_input()

--- danpy/test_sb.py
+++ danpy/test_sb.py
@@ -4,3 +4,10 @@ from .sb import *
 def test_double_input():
   output = double_input(2)
   assert output == 4, "Error with double_input."
+
+  from unittest.mock import patch, call
+
+@patch('builtins.print')
+def test_print_input(mocked_print):
+  test_input('This is a test.')
+  assert mocked_print.mock_calls == [call('This is a test.')]
