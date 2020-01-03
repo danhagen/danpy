77b52fb1f625a381f2b30654dd334eaf312ab2ca
dhagen.work <dhagen@usc.edu>
2020-01-03 10:00:48 -0800

    v 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if path.isfile(readme_filepath):
 
 setuptools.setup(
     name="danpy",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/danhagen/danpy",
     author="Daniel A Hagen",
     author_email="dhagen@usc.edu",
