e3550296ffb4e6dc6ea8700b60b4b8bfe0ea8e5b
danhagen <dhagen@usc.edu>
2020-02-27 12:54:49 -0800

    setup for 1.1.1

--- danpy/__init__.py
+++ danpy/__init__.py
@@ -1,9 +1,5 @@
 from .sb import *
 from .useful_functions import *
-<<<<<<< HEAD
-__version__ = '1.0.1'
-=======
-__version__ = '1.1.0'
->>>>>>> 145e09639f1497278d8f6da4156a72cb1777f2b9
+__version__ = '1.1.1'
 __author__ = 'Daniel Hagen <dhagen@usc.edu>'
 __all__ = []
